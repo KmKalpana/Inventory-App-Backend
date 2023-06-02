69a16ca35baa88776c94c7aab7bd50cfb204104f
Kalpana Kathait <KALPANAKATHAIT11@GMAIL.COM>
2023-06-02 10:31:05 +0530

    updated code

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const path=require("path")
 const contactRoute=require("./routes/contactRoute")
 const app = express()
 
-//middleware
+// Set the strictQuery option before connecting to MongoDB
+mongoose.set('strictQuery', false);
+
+// middleware
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
@@ -20,20 +23,19 @@ app.use(cors({
        credentials:true
 }))
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-//Routes Middleware
+// Routes Middleware
 app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
 
-//Routes
+// Routes
 app.get('/', (req, res) => {
   res.send('Home Page')
 })
 app.use("/api/contactus", contactRoute);
-//Error Middleware 
+// Error Middleware 
 app.use(errorHandler)
 const PORT = process.env.PORT || 5000
-//connect to mongodb and start server
-mongoose.set('strictQuery', false);
+// Connect to MongoDB and start the server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
